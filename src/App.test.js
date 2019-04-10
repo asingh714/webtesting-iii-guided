565cf934387b754a0fbd8565646b2793b7700fff
Aman <amsingh714@gmail.com>
2019-04-10 13:54:18 -0400

    added a few tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import renderer from 'react-test-renderer'; 
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "react-testing-library";
+import App, { Greeter } from "./App";
 
-import App from './App';
-
-describe('<App />', () => {
-  it("matches snapshot", () => {
+describe("<App />", () => {
+  // you can used it.only to only run one test.
+  it.skip("matches snapshot", () => {
     const tree = renderer.create(<App />).toJSON();
     // this is all done with jest. not react-test-renderer
     expect(tree).toMatchSnapshot();
-  })
+  });
+
+  // it.todo doesn't work because create react app doesn't use latest version of jest.
+});
+
+describe("mocking", () => {
+  it("is mocking me", () => {
+    const mock = jest.fn();
+
+    const result = mock();
+    mock();
+
+    expect(result).toBeUndefined();
+    expect(mock).toHaveBeenCalledTimes(2);
+  });
+
+  it("controls the mock", () => {
+    const mock = jest.fn(() => "hello");
+
+    const result = mock("smile");
+
+    expect(result).toBe("hello");
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveBeenCalledWith("smile");
+  });
+});
+
+describe("<Greet />", () => {
+  it("should save when clicking Save button", () => {
+    const saveMock = jest.fn();
+    const { getByText, queryByText } = render(<Greeter save={saveMock} />);
+
+    const saveButton = getByText(/save/i);
+    // expect(queryByText(/saving/i)).toBeFalsy();
+
+    fireEvent.click(saveButton);
+    // expect(queryByText(/saving/i)).toBeTruthy();
+
+    expect(saveMock).toHaveBeenCalledTimes(1);
+  });
 });
